bf5d6bde48667bf59253041af6c380717c132b53
Mark Shui Hu <watermarkhu@gmail.com>
2023-10-11 09:35:52 +0200

    Add test for read/write operations

---
+++ test/t61ReadWriteOperations.m
@@ -0,0 +1,16 @@
+% SYNTAX TEST "source.matlab"  "Imports: https://github.com/mathworks/MATLAB-Language-grammar/pull/51"
+
+variable
+% <------- variable.other.readwrite.matlab
+
+classInstance.property
+% <------------ variable.other.readwrite.matlab
+%            ^ punctuation.accessor.dot.matlab
+%             ^^^^^^^^ variable.other.property.matlab
+
+classInstance.method()
+% <------------ variable.other.readwrite.matlab
+%            ^ punctuation.accessor.dot.matlab
+%             ^^^^^^ entity.name.function.matlab
+%                   ^ punctuation.section.parens.begin.matlab
+%                    ^ punctuation.section.parens.end.matlab      
