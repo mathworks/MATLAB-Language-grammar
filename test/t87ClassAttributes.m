f4e2794e1949b605f1d72a93999c12761c87cede
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-07-18 14:06:29 +0200

    add test

---
+++ test/t87ClassAttributes.m
@@ -0,0 +1,5 @@
+classdef (AllowedSubclasses = {?SubClass1,?SubClass2}) SuperClass
+%        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ storage.modifier.section.class.matlab
+%                             ^^^^^^^^^^^^^^^^^^^^^^^ meta.cell.literal.matlab
+%                                                      ^^^^^^^^^^ entity.name.type.class.matlab
+end
