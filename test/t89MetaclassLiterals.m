9772a5a36bdb94ab79b81157ff005a78950304eb
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-07-19 10:59:18 +0200

    add unit test

---
+++ test/t89MetaclassLiterals.m
@@ -0,0 +1,17 @@
+% SYNTAX TEST "source.matlab"  "Property validation: https://github.com/mathworks/MATLAB-Language-grammar/issues/89"
+classdef (AllowedSubclasses = {?SubClass1,?namespace1.SubClass2}) SuperClass
+%                               ^^^^^^^^^ meta.metaclass.matlab entity.other.class.matlab
+%                                          ^^^^^^^^^^^^^^^^^^^^ meta.metaclass.matlab
+%                                          ^^^^^^^^^^ entity.name.namespace.matlab
+%                                                     ^^^^^^^^^ entity.other.class.matlab
+    methods
+        function foo()
+            x = ?namespace2.cls
+%                ^^^^^^^^^^^^^^ meta.metaclass.matlab
+%                ^^^^^^^^^^ entity.name.namespace.matlab
+%                           ^^^ entity.other.class.matlab
+            y = ?MyClass
+%                ^^^^^^^ meta.metaclass.matlab entity.other.class.matlab
+        end
+    end
+end
