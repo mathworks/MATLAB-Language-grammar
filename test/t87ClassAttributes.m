7de6a0ccdc89c0e8c25ed1488e4f814829793a5d
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-07-19 10:32:58 +0200

    fix test and allow comments in classdef line

--- test/t87ClassAttributes.m
+++ test/t87ClassAttributes.m
@@ -1,5 +1,6 @@
+% SYNTAX TEST "source.matlab"  "Property validation: https://github.com/mathworks/MATLAB-Language-grammar/issues/87"
 classdef (AllowedSubclasses = {?SubClass1,?SubClass2}) SuperClass
 %        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ storage.modifier.section.class.matlab
-%                             ^^^^^^^^^^^^^^^^^^^^^^^ meta.cell.literal.matlab
+%                              ^^^^^^^^^^^^^^^^^^^^^ meta.cell.literal.matlab
 %                                                      ^^^^^^^^^^ entity.name.type.class.matlab
 end
