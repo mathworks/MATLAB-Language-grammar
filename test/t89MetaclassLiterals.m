5f142da38f1bc24113fb6ab80a07948f3eb451ab
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2024-07-22 09:46:05 +0200

    fix name in test

--- test/t89MetaclassLiterals.m
+++ test/t89MetaclassLiterals.m
@@ -1,4 +1,4 @@
-% SYNTAX TEST "source.matlab"  "Property validation: https://github.com/mathworks/MATLAB-Language-grammar/issues/89"
+% SYNTAX TEST "source.matlab"  "Metaclasses: https://github.com/mathworks/MATLAB-Language-grammar/issues/89"
 classdef (AllowedSubclasses = {?SubClass1,?namespace1.SubClass2}) SuperClass
 %                               ^^^^^^^^^ meta.metaclass.matlab entity.other.class.matlab
 %                                          ^^^^^^^^^^^^^^^^^^^^ meta.metaclass.matlab
